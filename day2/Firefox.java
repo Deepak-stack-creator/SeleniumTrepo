2a554599b9a5586f57f807800f6135b9664bc3f2
Deepak-stack-creator <55042260+Deepak-stack-creator@users.noreply.github.com>
2025-11-08 18:45:41 +0530

    Add files via upload
    week 1 day 2

---
+++ day2/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserversion = 100.2f;
+		String browserName = "Firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo ='f';
+				
+		 System.out.println("Browser version:" + browserversion);
+		 System.out.println("Browser Name:" + browserName);
+		 System.out.println("Is Visiblee:" + isVisible);
+		 System.out.println("Release Year:" + releaseYear);
+		 System.out.println("Browser Logo:" + browserLogo);
+
+	}
+
+}
