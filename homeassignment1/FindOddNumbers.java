3df20784072f8ebb8836af4b06ccddb46c1c5a97
Deepak-stack-creator <55042260+Deepak-stack-creator@users.noreply.github.com>
2025-11-09 11:19:11 +0530

    Add files via upload

---
+++ homeassignment1/FindOddNumbers.java
@@ -0,0 +1,17 @@
+package week1.homeassignment1;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		 int maxRange = 10;  // step 1
+
+	        for (int i = 1; i <= maxRange; i++) {  
+
+	            if (i % 2 == 1) {   
+	                System.out.println(i); 
+	}
+
+}
+	        
+	}
+}
